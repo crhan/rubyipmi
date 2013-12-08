2107cb7417bf0f00dc0e41ca3e03bb444889769d
墨泪 <ruohan.chen@alipay.com>
2013-12-08 21:17:08 +0800

    bugfix: enable lanplus by default

--- lib/rubyipmi/version.rb
+++ lib/rubyipmi/version.rb
@@ -2,8 +2,8 @@ module Rubyipmi
   class VERSION
     MAJOR = 0
     MINOR = 7
-    PATCH = 3
-    PRE   = nil
+    PATCH = 4
+    PRE   = :pre
 
     def self.to_s
       [MAJOR, MINOR, PATCH, PRE].compact.join('.')
